ac512e4ca0bcb8d19f3245d74b32652940cf66ba
danmesfin <danielmsfn@gmail.com>
2023-03-01 04:17:54 +0300

    updated app.tsx

--- App.tsx
+++ App.tsx
@@ -1,38 +1,11 @@
-/* eslint-disable react/no-unstable-nested-components */
-import React, {useState} from 'react';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
 
 import {Provider} from 'react-redux';
 import store from './src/store/store';
 
 import App from './src/app';
-// import {useDispatch, useSelector} from 'react-redux';
-
-// import SignInScreen from './src/Screen/Auth/SignInScreen';
-// import SignupScreen from './src/Screen/Auth/SignUpScreen';
-
-// //import HomeScreen from './src/Screen/HomeScreen';
-// import CartScreen from './src/Screen/CartScreen';
-// import ProfileScreen from './src/Screen/ProfileScreen';
-// import HomeStackNavigator from './src/Navigators/HomeStackNavigator';
-// import {createStackNavigator} from '@react-navigation/stack';
-// //import CounterScreen from './src/Screen/CounterScreen';
-
-// const Tab = createBottomTabNavigator();
-// const Stack = createStackNavigator();
 
 const Navigation = () => {
-  //const {user} = useSelector(state => state.auth);
-  //const UserContext = createContext();
-  //const [user, setUser] = useState(null);
-  //const [isLogedin, setIsLogedIn] = useState(false);
-
-  // if (user) {
-  //   setIsLogedIn(true);
-  // }
-
   return (
     <Provider store={store}>
       <App />
